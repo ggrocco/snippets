6d862c05129ebc7a7e65bf9b5c0a8242221fa5aa
Georgeo Rocco <ggrocco@gmail.com>
2018-09-11 10:58:21 -0300

    Add scripts for kubernetes secrets manipulation

---
+++ k_apply_secret.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+# This script will encrypt and apply the secret file,
+# will need to have the namespace on.
+# Author: https://github.com/ggrocco
+# Last Change: 2018-09-11
+
+require 'base64'
+require 'yaml'
+require 'tempfile'
+
+INPUT = ARGV[0]
+VALID_EXT = '.yaml'.freeze
+
+unless INPUT && File.exist?(INPUT) && File.extname(INPUT) == VALID_EXT
+  puts 'Need to have a base secrets file for be encoded'
+  puts "Ex: $ #{__FILE__} BASE_SECRET_FILE.yaml"
+  exit 1
+end
+
+file = File.basename(INPUT)
+input_file = YAML.load_file(INPUT)
+data = input_file['data']
+
+unless data.is_a?(Hash)
+  puts 'This file is not a base secret file'
+  exit 1
+end
+
+puts "Processing the file #{file}"
+output = {}
+data.each do |key, value|
+  puts "Encoding secret: #{key.downcase}"
+  output[key.downcase] = Base64.encode64(value.to_s).delete("\n").strip
+end
+
+input_file['data'] = output
+
+file = Tempfile.new(file)
+file.write(input_file.to_yaml)
+file.close
+
+system("kubectl apply -f #{file.path}")
+file.unlink
+puts 'Done.'
---
+++ k_get_secret.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# This script will decrypt a secret file data and build
+# other based on the internal template.
+# Author: https://github.com/ggrocco
+# Last Change: 2018-09-11
+
+require 'base64'
+require 'yaml'
+
+SECRET_NAME = ARGV[0]
+NAMESPACE = ARGV[1] || 'default'
+
+unless SECRET_NAME && NAMESPACE
+  puts 'To download needs to have the secret name and namespace'
+  puts "Ex: $ #{__FILE__} SECRET_NAME NAMESPACE"
+  exit 1
+end
+
+YAML_TEMPLATE = <<-YAML.freeze
+  apiVersion: v1
+  kind: Secret
+  metadata:
+    name: #{SECRET_NAME}
+    namespace: #{NAMESPACE}
+  type: Opaque
+  data:
+YAML
+
+output_file = YAML.safe_load(YAML_TEMPLATE)
+output_file['data'] = {}
+
+file = `kubectl get secret #{SECRET_NAME} -n #{NAMESPACE} -o jsonpath='{.data}'`
+if file.empty?
+  puts 'Fail on retrieve the secret'
+  exit 1
+end
+file[4...-1].split(' ').map do |line|
+  key, value = line.split(':')
+  output_file['data'][key] = value ? Base64.decode64(value) : nil
+end
+
+puts output_file.to_yaml
